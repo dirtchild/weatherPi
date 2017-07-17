64817cbab1e8cc54f747a62287f5298d797dcb9b
dirtchild <git@dirtchild.net>
2017-07-17 11:23:19 +0100

    data structures

--- weatherSensors/SensorData.py
+++ weatherSensors/SensorData.py
@@ -6,13 +6,20 @@
 ## * label (string): the title - UID for this measurement
 ## * value (int): the reading
 ## * unit (string): what we are measuring 
+## * timestamp: when it was created/taken
 ##
 ## CREATED: 
 ## MODIFIED: see https://github.com/dirtchild/weatherPi
 ##
 
-class SensorData:
-    def __init__(self, label, value, unit):
-	    self.label = label
+import time
+
+class SensorReading:
+	def __init__(self, label, value, unit, timestamp=None):
+		self.label = label
 		self.value = value
 		self.unit = unit
+		if timestamp is None:
+			self.timestamp = time.time()
+		else:	
+			self.timestamp = timestamp
