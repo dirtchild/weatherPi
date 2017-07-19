4ff684e67d4530266757353fb7f34629d7dbafee
Christopher McLean <mapcjmc@leeds.ac.uk>
2017-07-19 08:26:29 +0100

    correctly sourcing people I've borrowed from

--- weatherSensors/DHT11.py
+++ weatherSensors/DHT11.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 # reads data from an adafruit DHT11 temp/humidity sensor
+# SOURCES:
+#       * https://github.com/ControlEverythingCommunity/MPL3115A2/blob/master/Python/MPL3115A2.py
+#       * https://github.com/johnwargo/pi_weather_station_simple
+#       * https://www.domoticz.com/forum/viewtopic.php?t=839
 # RETURNS: two objects for humidity and temperature
 # CREATED: 2017-07-18
 # MODIFIED: see https://github.com/dirtchild/weatherPi
