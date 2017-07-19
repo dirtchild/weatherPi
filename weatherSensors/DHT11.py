d9dc1bb23a47903af43cfb5bcd48e1c6ee6ef3c2
Christopher McLean <mapcjmc@leeds.ac.uk>
2017-07-19 14:49:21 +0100

    forgot to bring existing sensors into line with the wunderground data headers

--- weatherSensors/DHT11.py
+++ weatherSensors/DHT11.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 # reads data from an adafruit DHT11 temp/humidity sensor
+# labels follow those set out in the Wunderground PWS API: 
+#	http://wiki.wunderground.com/index.php/PWS_-_Upload_Protocol
+#
 # SOURCES:
 #       * https://github.com/ControlEverythingCommunity/MPL3115A2/blob/master/Python/MPL3115A2.py
 #       * https://github.com/johnwargo/pi_weather_station_simple
@@ -18,5 +21,5 @@ def getDHT11():
 
 	hum, tem = Adafruit_DHT.read_retry(11, self.gpio)
 	dhtHum = SensorReading("dht11", "humidity", hum, "%")
-	dhtTem = SensorReading("dht11", "temperature", tem, "f")
+	dhtTem = SensorReading("dht11", "tempf", tem, "f")
 	return (dhtTem,dhtHum)
