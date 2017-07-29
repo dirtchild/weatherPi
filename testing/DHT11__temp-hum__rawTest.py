89837cb17a1583d41e15538b5bf2e78b809e37eb
dirtchild <github@dirtchild.net>
2017-07-29 13:19:12 +0000

    testing more of the hardware

--- testing/DHT11__temp-hum__rawTest.py
+++ testing/DHT11__temp-hum__rawTest.py
@@ -8,5 +8,5 @@ import sys
 import Adafruit_DHT
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(11, 23)
+    humidity, temperature = Adafruit_DHT.read_retry(11, 5)
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
