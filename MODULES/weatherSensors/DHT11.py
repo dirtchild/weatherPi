73fe3c8f3bde4bb492881899f282f2b51b84b910
dirtchild <github@dirtchild.net>
2017-08-04 18:32:32 +0000

    working on DHT11

--- MODULES/weatherSensors/DHT11.py
+++ MODULES/weatherSensors/DHT11.py
@@ -8,6 +8,7 @@
 #       * https://github.com/ControlEverythingCommunity/MPL3115A2/blob/master/Python/MPL3115A2.py
 #       * https://github.com/johnwargo/pi_weather_station_simple
 #       * https://www.domoticz.com/forum/viewtopic.php?t=839
+#		* https://github.com/adafruit/Adafruit_Python_DHT/blob/master/examples/AdafruitDHT.py
 # RETURNS: two objects for humidity and temperature
 # CREATED: 2017-07-18
 # MODIFIED: see https://github.com/dirtchild/weatherPi
@@ -16,14 +17,14 @@ import Adafruit_DHT
 from SensorData import SensorReading 
 
 def getReading():
-	# pin the dht is connected to
-	gpio = 23
-	sensor = Adafruit_DHT.DHT11	
-
-	hum, tem = Adafruit_DHT.read_retry(sensor, gpio)
-	#DEBUG
-	print hum
-	print tem
+	#NOT WORKING: hum, tem = Adafruit_DHT.read_retry(sensor, gpio)
+	#WORKINGISH: hum, tem = Adafruit_DHT.read_retry(11, 5)
+	hum, temC = Adafruit_DHT.read_retry(11, 5)
+	CONVERT temC TO FARENHEIGHT
 	dhtHum = SensorReading("dht11", "humidity", hum, "%")
 	dhtTem = SensorReading("dht11", "tempf", tem, "f")
 	return (dhtTem,dhtHum)
+
+
+
+
