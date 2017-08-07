f11a7c9cafe8ea35cf37fccca27af9e57e248f05
dirtchild <git@dirtchild.net>
2017-08-07 12:13:18 +0100

    main script work. refactoring a big chunk of it. commiting to save state

--- weatherPi.py
+++ weatherPi.py
@@ -14,79 +14,106 @@
 from weatherSensors import *
 from config import *
 import time
-
+import MySQLdb as my
 
 # fire off our time-dependent sensors (wind speed, rainfall etc)
 wSpeed = eventSensor(W_SPD_GPIO, W_SPD_CALIBRATION, "wind speed events", "w_spd", "MPH", W_SPD_EVENTS_PERIOD)
 rain = eventSensor(RAIN_GPIO, RAIN_CALIBRATION, "rain events", "rain", "mm", RAIN_EVENTS_PERIOD)
 
+# DB connection
+db = my.connect(host=db_host,user=db_user,passwd=db_pwd,db=db_db)
+dbCursor = db.cursor()
+
+# URL prep stuff for wunderground upload
+# DEBUG
+
 # loop forever.
 while True:
-	# read in all of our single check sensors
+	# read in all of our single check sensors for thsi run
 	dhtTem,dhtHum = weatherSensors.DHT11.getReading()
 	mplTem,mplPres,mplAlt = weatherSensors.MPL3115A2.getReading()
 	uv = weatherSensors.uv.getReading()
 	windDir = weatherSensors.windDirection.getReading()
+	windSpeedNow = wSpeed.getLast()
+	windSpdMph_avg2m = wSpeed.getPeriodAverage("windSpdMph_avg2m",120)
+	rainIn = mmToInches(rain.getPeriodTotal("rainIn", 3600))
+	dailyRainIn = mmToInches(rain.getPeriodTotal("dailyRainIn", 86400))
 
 	# calculate a few thigns
 	tempF = (mplTem + dhtTem) / 2
 	dpF = dewpointF(tempF, dhtHum)
 
+	# setup our string to write to things base don what we've gathered
+
+
+
+#	winddir = 
+#	windspeedmph =
+#	windgustmph = "Null"
+#	windgustdir = "Null"
+#	windspdmph_avg2m = 
+#	winddir_avg2m = "Null"
+#	windgustmph_10m = "Null"
+#	windgustdir_10m = "Null"
+#	humidity =
+#	dewptf = 
+#	tempf =
+#	rainin =
+#	dailyrainin =
+#	baromin =
+#	weather = "Null"
+#	clouds = "Null"
+#	soiltempf = "Null"
+#	soilmoisture = "Null"
+#	leafwetness = "Null"
+#	solarradiation = "Null"
+#	UV = 
+#	visibility = "Null"
+#	indoortempf = "Null"
+#	indoorhumidity = "Null"
+	
 	# do the work
 	if WEATHER_UPLOAD == True:
-		#DEBUG
-		print "[DEBUG]	writing stuff to online stuff"
 		# write to wunderground
-
-		# write to our database
-	else:
-		print "[DEBUG]	Not sending data to online services"
+		#DEBUG
+		print "[DEBUG]	writing stuff to wunder"
 		
+		# write to our database
+#		sql = "insert into %s (%s,%s) VALUES (%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d)" % \
+#			(db_table,\
+#			winddir,\
+#			windspeedmph,\
+#			windgustmph,\
+#			windgustdir,\
+#			windspdmph_avg2m,\
+#			winddir_avg2m,\
+#			windgustmph_10m,\
+#			windgustdir_10m,\
+#			humidity,\
+#			dewptf,\
+#			tempf,\
+#			rainin,\
+#			dailyrainin,\
+#			baromin,\
+#			weather,\
+#			clouds,\
+#			soiltempf,\
+#			soilmoisture,\
+#			leafwetness,\
+#			solarradiation,\
+#			UV,\
+#			visibility,\
+#			indoortempf,\
+#			indoorhumidity)
+#		number_of_rows = cursor.execute(sql)
+#		db.commit()
+	
 	# log something?
 	print "[DEBUG] log to logFile"
 	
 	# wait on a bit
 	time.sleep(readInterval)
 
+# clean up after ourselves
+db.close()
 
-###################################################################################
-# WUNDERGROUP API REFERENCE
-# 
-# http://wiki.wunderground.com/index.php/PWS_-_Upload_Protocol
-#
-#-----FIELDS-----
-# action=updateraw
-# ID
-# PASSWORD
-# dateutc=now
-# winddir - [0-360 instantaneous wind direction]
-# windspeedmph - [mph instantaneous wind speed]
-# windgustmph - [mph current wind gust, using software specific time period]
-# windgustdir - [0-360 using software specific time period]
-# windspdmph_avg2m  - [mph 2 minute average wind speed mph]
-# winddir_avg2m - [0-360 2 minute average wind direction]
-# windgustmph_10m - [mph past 10 minutes wind gust mph ]
-# windgustdir_10m - [0-360 past 10 minutes wind gust direction]
-# humidity - [% outdoor humidity 0-100%]
-# dewptf- [F outdoor dewpoint F]
-# tempf - [F outdoor temperature] 
-#  * for extra outdoor sensors use temp2f, temp3f, and so on
-# rainin - [rain inches over the past hour)] -- the accumulated rainfall in the past 60 min
-# dailyrainin - [rain inches so far today in local time]
-# baromin - [barometric pressure inches]
-# weather - [text] -- metar style (+RA)
-# clouds - [text] -- SKC, FEW, SCT, BKN, OVC
-# soiltempf - [F soil temperature]
-#  * for sensors 2,3,4 use soiltemp2f, soiltemp3f, and soiltemp4f
-# soilmoisture - [%]
-# * for sensors 2,3,4 use soilmoisture2, soilmoisture3, and soilmoisture4
-# leafwetness  - [%]
-# + for sensor 2 use leafwetness2
-# solarradiation - [W/m^2]
-# UV - [index]
-# visibility - [nm visibility]
-# indoortempf - [F indoor temperature F]
-# indoorhumidity - [% indoor humidity 0-100]
-# 
-#-----EXAMPLE URI UPLOAD:-----
-# https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?ID=KCASANFR5&PASSWORD=XXXXXX&dateutc=2000-01-01+10%3A32%3A35&winddir=230&windspeedmph=12&windgustmph=12&tempf=70&rainin=0&baromin=29.1&dewptf=68.2&humidity=90&weather=&clouds=&softwaretype=vws%20versionxx&action=updateraw# 
