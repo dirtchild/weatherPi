3c86fad9a9aa6f9e9fbeda357ef6712398c3ef95
dirtchild <github@dirtchild.net>
2017-10-28 18:01:48 +0000

    fixing bits for wifi logging via cron

--- logWifiStrength.sh
+++ logWifiStrength.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-( echo -n `date +"[%Y-%m-%d %H:%M:%S]"`;iwconfig wlan0 | grep Link ) >> /var/log/wifi-quality.log
+( echo -n `date +"[%Y-%m-%d %H:%M:%S]"`;/sbin/iwconfig wlan0 | grep Link ) >> /var/log/wifi-quality.log
+
 
 
